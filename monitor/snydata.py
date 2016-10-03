5b9aa22165eb82fb91515a47f53ef120f509f682
gaoyangxiaozhu <1456543560@qq.com>
2016-10-03 09:48:51 +0800

    ignore *.pyc and delete *.pyc in repository

--- monitor/snydata.py
+++ monitor/snydata.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 '''
-Created on 2014��6��9��
+Created on 2014
 
 @author: zcan
 '''
@@ -17,17 +17,17 @@ mysql_host="10.103.242.128"
 max_record_table_num=10 #监控数据最大保留天数
 ########################################
 class backupServer(threading.Thread):
-    
+
     def __init__(self,data,tableName,tableKeeper):
         threading.Thread.__init__(self)
         self.__data=data
         self.__tableName=tableName
         self.__tableKeeper=tableKeeper
-                      
+
     #格式化为tuple格式数据(uuid,time,data)
     def __formatData(self,data):
         return (self.__tableName,data[1],int(string.atof(data[2])),data[0])
-                
+
     def run(self):
         conn=self.__tableKeeper.getconnect()
         if conn:
@@ -39,21 +39,21 @@ class backupServer(threading.Thread):
                     cur.execute('''insert into %s(uuid,time,data) values('%s',%f,'%s')'''%formatData)
             else:
                 formatData=self.__formatData(dataTmp)
-                cur.execute('''insert into %s(uuid,time,data) values('%s',%f,'%s')'''%formatData)                
+                cur.execute('''insert into %s(uuid,time,data) values('%s',%f,'%s')'''%formatData)
             conn.commit()
             cur.close()
         else:
             print "updata fail because there is a error..."
         self.__tableKeeper.close()
-            
+
 class tableKeeper():
     def __init__(self):
         self.__tableName=None
         self.__connect=None
-        
+
     def __removeTableByNum(self,cur):
         #开始删除多余的表
-        cur.execute("show tables like 'instance%'")                
+        cur.execute("show tables like 'instance%'")
         tables=cur.fetchall()
         remove_num=len(tables)-max_record_table_num
         if remove_num:
@@ -64,11 +64,11 @@ class tableKeeper():
             for i in range(remove_num):
                 cur.execute("drop table %s"%table_list[i])
                 print "delete table",table_list[i]
-                
+
     def  getconnect(self):
         if self.__connect and self.__connect.open==True:
             return self.__connect
-        
+
         connTime=0;
         conn=None
         while not conn and connTime<5:
@@ -79,15 +79,15 @@ class tableKeeper():
                                      passwd=mysql_passwd,
                                      db=mysql_database,
                                      port=mysql_port)
-            except MySQLdb.Error, e:  
+            except MySQLdb.Error, e:
                 print "Error %d:%s" % (e.args[0], e.args[1]),"try again to get table name %d"%connTime
             time.sleep(3)
         return conn
-    
+
     def close(self):
         if self.__connect and self.__connect.open==True:
             self.__connect.close()
-            
+
     def __check(self,conn):
 
         #开始检测是否存在
@@ -109,12 +109,12 @@ class tableKeeper():
             cur.close()
             return tableName
         return None
-        
+
     def getTableName(self, conn):
         if not self.__tableName or self.__tableName.find(time.strftime("%Y%m%d"))<0:
             self.__tableName=self.__check(conn)
         return self.__tableName
-    
+
 
 if __name__ =="__main__":
     '''
@@ -127,4 +127,3 @@ if __name__ =="__main__":
 #     print cur.fetchall()
 #     cur.close()
 #     con.close()
-        
